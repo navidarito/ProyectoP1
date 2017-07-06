9be67b2b2fafaff51ebc854a56f503892337fb6d
wallygr97 <roodwally@gmail.com>
2017-07-06 10:51:11 -0400

    creacion de la clase cliente

---
+++ ProyectoTiendaComputadora/src/logica/Cliente.java
@@ -0,0 +1,50 @@
+package logica;
+
+public class Cliente {
+	private String nombre;
+	private String cedula;
+	private String direccion;
+	private String telefono;
+	
+	public Cliente(String nombre, String cedula, String direccion, String telefono) {
+		super();
+		this.nombre = nombre;
+		this.cedula = cedula;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+
+}
